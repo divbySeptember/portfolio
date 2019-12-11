913b981eb2ff4c5c4543bad8233ee77fa820df5b
divbyseptember <beatsbyseptember@gmail.com>
2019-12-11 15:54:57 -0500

    Handle Submit on Contact

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -4,12 +4,20 @@ import {Form, Button} from 'react-bootstrap'
 import './index.css'
 
 const Contact = () => {
+
+
+
+    const handlesubmit = (e) => {
+        e.preventDefault()
+    }
+
+
     return(
         <div className='contactMe'>
             <img src={Contacts} />
             <div className='contactText'>
                 <h1 className='newHeader'> ~ Contact<span className='folio'>Me</span> ~</h1>
-                <Form name="contact" netlify>
+                <Form name="contact" onSubmit={handleSubmit} netlify>
                 <Form.Group controlId="formBasicEmail">
                    
                     <Form.Control type="email" placeholder="Enter email" />
